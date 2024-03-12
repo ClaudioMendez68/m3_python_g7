42b3be88aef319b80ed5ed45a27c5dae8372cf18
Claudio Mendez <claudio.mendez.online@gmail.com>
2024-03-12 12:48:54 -0300

    Probando sintaxis de Python

--- dia_1/ejemplo_1.py
+++ dia_1/ejemplo_1.py
@@ -1,1 +1,18 @@
-print('Hola Mundo')
+# INTERPOLACIÓN
+num = 2+2
+nombre = "Claudio"
+
+print (f"1. El número es: {num}")
+print (f"2. El número es: {num+2}")
+print ("3. Tu nombre es \n"+nombre)
+
+# string.format
+print ("4. Tu nombre es {} y el número es {}".format(nombre, num))
+
+# formato con %: %s para string y %d para número
+print ("5. Tu nombre es %s y el número es %d"%(nombre, num))
+
+# Método con cadena
+print (nombre.title())
+titulo = "pytHon ForEver"
+print(titulo.title())
