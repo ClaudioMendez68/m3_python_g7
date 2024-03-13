0cb044909dafa9ea36ab813dee2874b99e5d2f23
Claudio Mendez <claudio.mendez.online@gmail.com>
2024-03-13 13:07:23 -0300

    ejemplos dia 2

---
+++ dia_2/operadores_librerias.py
@@ -0,0 +1,12 @@
+import math
+
+print(2**4) # potencia
+print(5/2) # división
+print(5//2) # división entera
+print(5%2) # módulo
+
+print(math.pow(2,4)) # potencia, resultado tipo float
+print(math.sqrt(16)) # raíz cuadrada
+
+edad = int(input("Ingrese su edad: "))
+print(f"La edad ingresada es: {edad}")
