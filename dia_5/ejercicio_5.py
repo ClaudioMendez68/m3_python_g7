e1edc5c6aab72061c8113cac7971fe181945d67e
Claudio Mendez <claudio.mendez.online@gmail.com>
2024-03-18 18:45:50 -0300

    ejercicio de pruebas lógicas

---
+++ dia_5/ejercicio_5.py
@@ -0,0 +1,17 @@
+nombre= "Eduardo"
+me_llamo_claudio= nombre == "Claudio"
+
+'''
+    Soy estudiante Y ingreso a clase todos los días
+        
+    P = Soy estudiante
+    Q = Ingreso a clase todos los días
+        
+    P Q AND
+    V V  V
+    V F  F
+    F V  F
+    F F  F
+        
+    Me gusta comer frutas O me gusta comer verduras
+'''
